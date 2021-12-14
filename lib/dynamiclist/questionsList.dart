f8be288ee2c6c5dfbba42206c2ee0d44d7babf8d
LimonDP <limonraycse2020@gmail.com>
2021-12-14 22:39:02 +0600

    first commit

---
+++ lib/dynamiclist/questionsList.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+List<String> questions = [
+  '((2 ^ 2)^ 3) = 25',
+  ' 5 > 10 or 5 < 7',
+  '30% of x is equal to 0.03x',
+  ' "x is at most equal to 9" is written mathematically as x < 9.',
+  '3^20 + 3^20 + 3^20 = 3^21',
+  '1000/0 = 0.',
+  '0/1000 = 0.',
+  '0.00000010 = 1.',
+  '1 / (( -2 ) -4) = 16.',
+  'x = 7 DOES NOT satisfy the inequality x - 7 < 0'
+];
+
+List<bool> answers = [
+  false,
+  true,
+  false,
+  false,
+  true,
+  false,
+  true,
+  true,
+  true,
+  true
+];
+
+List<Icon> gusteranswer = [];
